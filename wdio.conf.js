c565e06240543d6d46bfec0eeff2430765137323
Patricia Cristina <patcrisaragao@gmail.com>
2022-09-10 18:29:57 -0400

    adicionando reports

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
 const {join} = require('path')
+const allure = require('allure-commandline')
+const video = require('wdio-video-reporter');
 
 exports.config = {
     hostname: 'localhost',
@@ -15,5 +17,43 @@ exports.config = {
         "automationName": "UiAutomator2",
         "app": join(process.cwd(), './app/android/loja-ebac.apk'),
         "appWaitActivity": "com.woocommerce.android/.ui.login.LoginActivity"
+    }],
+    waitForTimeout: 20000,
+    mochaOpts:{
+        timeout:300000
+    },
+    reporters: ['spec',
+    ['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
     }]
+    [video, {
+        saveAllVideos: true,       // If true, also saves videos for successful test cases
+        videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+      }],
+    ],
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
+    afterStep: async function (step, scenario, { error, duration, passed }, context) {
+        driver.takeScreenshot();
+    }
 }
